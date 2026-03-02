543c8d94fa0207880337b0dc4e24a6ece4edafd5
melvitti <vitti.mel02@gmail.com>
2026-03-01 21:39:52 -0300

    v2.0 - Structured access control system with multiple users using list and dictionary

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# == Initial ==
+print("Secure Access Control System")
 
-print("Secure Acces Control System")
+users = []  # list of users
 
-# Part one: Age Validation
+while True:
 
-name= input("Enter your name: ")
-age= int(input("Now, enter your age: "))
+    # User's data
+    name = input("Enter your name: ")
+    age = int(input("Now, enter your age: "))
 
-if age < 12:
-    print(f"Hi, {name}! We are sorry but you do not meet the age requirement.")
-else:
-    print(f"Hi, {name}! You meet the age requirement.")
+    registered = input("Are you registered? (yes/no): ").strip().lower()
+    vaccinated = input("Are you vaccinated? (yes/no): ").strip().lower()
+
+    # Creating user's dictionary
+    new_user = {
+        "name": name,
+        "age": age,
+        "registered": registered,
+        "vaccinated": vaccinated
+    }
+
+    # Adding to the list
+    users.append(new_user)
+
+    # Access verification
+    if age >= 12 and registered == "yes" and vaccinated == "yes":
+        print(f"Access authorized. Welcome, {name}!")
+    else:
+        print(f"Access denied. We're sorry, {name}.")
+
+    # Continue option
+    continue_option = input("Do you want to add another user account? (yes/no): ").strip().lower()
+
+    if continue_option != "yes":
+        break
+
+# Final Report
+print("\nFinal Report:")
+for user in users:
+    print(user)
 
 
 
